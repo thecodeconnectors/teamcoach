d7064dec47ecd9f4324d8eaaa39143a3568a1a10
Martijn van Nieuwenhoven <martijn@novat.io>
2024-02-06 19:01:32 +0100

    remove console.log

--- resources/assets/app/src/app/settings/settings.api.js
+++ resources/assets/app/src/app/settings/settings.api.js
@@ -1,7 +1,7 @@
 import api from '@/framework/api';
 
 export function getSettings() {
-    return api.get(`settings`);
+    return api.get('settings');
 }
 
 export function updateSettings(key, payload) {
--- resources/assets/app/src/framework/routes/middleware/rules/guest.js
+++ resources/assets/app/src/framework/routes/middleware/rules/guest.js
@@ -2,6 +2,5 @@ import {useStore} from '@/framework/store';
 
 export default (to, from, next) => {
     const store = useStore();
-    console.log('guest middleware');
     store.isAuthenticated ? next({name: 'home'}) : next();
 }
