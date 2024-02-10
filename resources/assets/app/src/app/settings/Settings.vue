c16ee95394c09a2794a0d967a72e933b59a684b6
Martijn van Nieuwenhoven <martijn@novat.io>
2024-02-11 00:20:39 +0100

    ssss

--- resources/assets/app/src/app/settings/Settings.vue
+++ resources/assets/app/src/app/settings/Settings.vue
@@ -4,8 +4,8 @@
     </div>
     <div class="max-w-full mx-auto px-4 sm:px-6 md:px-8 pt-6 lg:grid lg:gap-8">
         <main>
-            <div class="shadow sm:rounded-md sm:overflow-hidden mb-6">
-                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="sm:shadow sm:rounded-md sm:overflow-hidden mb-6">
+                <div class="py-5 bg-white space-y-6 sm:p-6">
                     <h2 class="text-lg leading-6 font-medium text-gray-900 mb-3">Game settings</h2>
                     <InputField type="number" min="1" max="10" step="1" @input="saveSetting('default_game_parts')" id="default_game_parts" v-model.lazy="store.settings.default_game_parts" label="Default game parts" />
                     <InputField type="number" min="1" step="1" @input="saveSetting('default_part_duration')" id="default_part_duration" v-model.lazy="store.settings.default_part_duration" label="Default part duration" />
